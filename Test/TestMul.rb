310328e0b87d2d82e5ed27cfbc30030b8684d18e
root <root@DESKTOP-9VPKLSN.localdomain>
2019-10-10 23:05:45 -0300

    comentarios prontos

--- Test/TestMul.rb
+++ Test/TestMul.rb
@@ -10,18 +10,21 @@ class Test_Mul < Test::Unit::TestCase
         mul = Mul.new(lit, lit1)
         assert_equal 150, mul.eval
     end
+    
     def test_mul2
         lit = Literal.new(10)
         lit1 = Literal.new(15)
         mul = Mul.new(lit, lit1)
         assert_equal "(10*15)", mul.print
     end
+    
     def test_mul3
         lit = Literal.new(3156)
         lit1 = Literal.new(548)
         mul = Mul.new(lit, lit1)
         assert_equal 1729488, mul.eval
     end
+    
     def test_mul4
         lit = Literal.new(3156)
         lit1 = Literal.new(548)
